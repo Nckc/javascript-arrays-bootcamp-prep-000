6b0503cdfc1dfaa6d3da42d93fa98039c1f0a262
Nat C <natcarn@gmail.com>
2018-03-07 07:58:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
 (chocolateBars[1],"foo");
-  return chocolateBars
+  return chocolateBars;
 }
 
 addElementToBeginningOfArray();
